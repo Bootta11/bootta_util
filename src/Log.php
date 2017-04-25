64251e4b5e2b79733831e7978e26c7eb2b59d848
Nebojsa <nebojsa.ristic@financ.ba>
2017-04-25 13:55:01 +0200

    Removed Log class reference to Config class. Now log is idenpendant

--- src/Log.php
+++ src/Log.php
@@ -4,22 +4,15 @@ namespace Bootta\Util;
 
 date_default_timezone_set('Europe/Sarajevo');
 
-require __DIR__ . '/../../vendor/autoload.php';
-
-use Bootta\Util\Config;
-
 class Log {
 
     private static $log_location;
     private static $update_interval = 60;
     private static $last_update_time;
 
-    public static function init() {
-        if (!Log::$log_location || (time() - Log::$last_update_time) > Log::$update_interval) {
-            Log::$last_update_time = time();
-            echo Log::$log_location;
-            Log::$log_location = __DIR__ . "/../../" . Config::get_global('log_location');
-        }
+    public static function init($path) {
+
+        Log::$log_location = $path;
     }
 
     /**
@@ -33,32 +26,42 @@ class Log {
      * @return bool If true log is saved successfully otherwise not
      */
     public static function info($msg, $module = "", $logs_file_by_date = true) {
-        Log::init();
-        $log_file_date = date("Y-m-d");
-        $location_log_info = Log::$log_location;
-        $called_from = ($_SERVER["SCRIPT_FILENAME"] ? $_SERVER["SCRIPT_FILENAME"] : "");
-        $date = date("Y-m-d h:i:s");
-        $level = "Info";
+        if ($log_location) {
+            $log_file_date = date("Y-m-d");
+            $location_log_info = Log::$log_location;
+            $called_from = ($_SERVER["SCRIPT_FILENAME"] ? $_SERVER["SCRIPT_FILENAME"] : "");
+            $date = date("Y-m-d h:i:s");
+            $level = "Info";
 
-        $message = "[{$date}] [{$called_from}] [{$level}] " . $msg . PHP_EOL;
+            $message = "[{$date}] [{$called_from}] [{$level}] " . $msg . PHP_EOL;
 
-        $result = error_log($message, 3, $location_log_info . "/" . ($module && strlen($module) > 0 ? $module . "_" : "") . ($logs_file_by_date ? "info" . $log_file_date . ".txt" : "info.txt"));
+            $result = error_log($message, 3, $location_log_info . "/" . ($module && strlen($module) > 0 ? $module . "_" : "") . ($logs_file_by_date ? "info" . $log_file_date . ".txt" : "info.txt"));
 
-        return $result;
+            return $result;
+        } else {
+            throw LogNotInitializedException();
+        }
     }
 
     public static function error($msg, $module = "", $logs_file_by_date = true) {
-        Log::init();
-        $log_file_date = date("Y-m-d");
-        $location_log_error = Log::$log_location;
-        $date = date("Y-m-d h:i:s");
-        $level = "Error";
-        $called_from = ($_SERVER["SCRIPT_FILENAME"] ? $_SERVER["SCRIPT_FILENAME"] : "");
-        $message = "[{$date}] [{$called_from}] [{$level}] " . $msg . PHP_EOL;
+        if ($log_location) {
+            $log_file_date = date("Y-m-d");
+            $location_log_error = Log::$log_location;
+            $date = date("Y-m-d h:i:s");
+            $level = "Error";
+            $called_from = ($_SERVER["SCRIPT_FILENAME"] ? $_SERVER["SCRIPT_FILENAME"] : "");
+            $message = "[{$date}] [{$called_from}] [{$level}] " . $msg . PHP_EOL;
 
-        error_log($message, 3, $location_log_error . "/" . ($module && strlen($module) > 0 ? $module . "_" : "") . ($logs_file_by_date ? "error" . $log_file_date . ".txt" : "error.txt"));
-        return $result;
+            error_log($message, 3, $location_log_error . "/" . ($module && strlen($module) > 0 ? $module . "_" : "") . ($logs_file_by_date ? "error" . $log_file_date . ".txt" : "error.txt"));
+            return $result;
+        } else {
+            throw LogNotInitializedException();
+        }
     }
+
+}
+
+class LogNotInitializedException extends Exception {
 
 }
 
